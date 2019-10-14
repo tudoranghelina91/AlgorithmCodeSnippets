8513ba76f39d21b9bf081f93481a4ae06f696ba5
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-10-14 10:29:34 +0300

    Update EvaluatingExpressionTree.cs

--- Binary Trees/EvaluatingExpressionTree.cs
+++ Binary Trees/EvaluatingExpressionTree.cs
@@ -2,19 +2,19 @@
         {
             // if node is multiplication operator
             if (root.Value == '*')
-                return Evaluate(root.Left) * Calculate(root.Right);
+                return Evaluate(root.Left) * Evaluate(root.Right);
             // if node is division operator
             else if (root.Value == '/')
-                return Evaluate(root.Left) / Calculate(root.Right);
+                return Evaluate(root.Left) / Evaluate(root.Right);
             // if node is modulo operator
             else if (root.Value == '%')
-                return Evaluate(root.Left) % Calculate(root.Right);
+                return Evaluate(root.Left) % Evaluate(root.Right);
             // if node is addition operator
             else if (root.Value == '+')
-                return Evaluate(root.Left) + Calculate(root.Right);
+                return Evaluate(root.Left) + Evaluate(root.Right);
             // if node is substraction operator
             else if (root.Value == '-')
-                return Evaluate(root.Left) - Calculate(root.Right);
+                return Evaluate(root.Left) - Evaluate(root.Right);
             else
                 return Convert.ToInt32(root.Value.ToString());
         }
